e43659076f66a83e753471d44d6bd9ca13a78247
Andrew Wong <wonga@valassis.com>
2022-04-14 13:28:04 -0500

    ICON-5387: Add timelapse feature to globe, new styling changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import "./App.css";
+import "./index.css";
 import Globe from "react-globe.gl";
 import { useEffect, useRef, useState } from "react";
 import { parseCountries } from "./parseCountries";
+
+import Slider from "@mui/material/Slider";
+import { IconButton } from "@mui/material";
+import { PlayCircle } from "@mui/icons-material";
+import { MAX_YEAR, MIN_YEAR, TIMELAPSE_INTERVAL } from "./constants";
+
+/*
+
+THINGS TO DO:
+
+- style slider
+- style year text
+- refine scale of datapoints (log)
+- make dropdown for different data (co2 VS co2_per_capita)
+
+*/
+
+const getAltitudeCalculationFunction = (year) => (feat) =>
+  Math.max(
+    0.05,
+    getBaseLog(3, +feat.properties.CO2_DATA_BY_YEAR?.[year]?.co2) * 10e-2 -
+      10e-2
+  );
+
+function getBaseLog(x, y) {
+  return Math.log(y) / Math.log(x);
+}
 
 function App() {
   const globeEl = useRef();
   const [countries, setCountries] = useState({ features: [] });
   const [altitude, setAltitude] = useState(0.1);
   const [transitionDuration, setTransitionDuration] = useState(1000);
+  const [year, setYear] = useState(2020);
+  const timeouts = useRef([]);
+
+  // Keep track of the latest year to handle edge case when user selects a year before the globe fully renders
+  const latestYear = useRef(year);
+  useEffect(() => {
+    latestYear.current = year;
+  });
 
   useEffect(() => {
     Promise.all([fetch("./countries.geojson"), fetch("./owid-co2-data.json")])
@@ -19,15 +55,8 @@ function App() {
         setCountries(parsedCountries);
 
         setTimeout(() => {
-          setTransitionDuration(4000);
-          setAltitude(
-            () => (feat) =>
-              Math.max(
-                0.1,
-                +feat.properties.CO2_DATA_BY_YEAR?.[2018]?.co2_per_capita * 7e-2
-              )
-          );
-        }, 3000);
+          setAltitude(() => getAltitudeCalculationFunction(latestYear.current));
+        }, 1000);
       });
   }, []);
 
@@ -39,22 +68,86 @@ function App() {
     globeEl.current.pointOfView({ altitude: 4 }, 5000);
   }, []);
 
+  const clearTimeouts = () => {
+    for (const timeout of timeouts.current) {
+      clearTimeout(timeout);
+    }
+    timeouts.current = [];
+  };
+
+  const changeYear = (e) => {
+    clearTimeouts();
+
+    const newYear = e.target.value;
+    setYear(newYear);
+    setTransitionDuration(100);
+    setAltitude(() => getAltitudeCalculationFunction(newYear));
+  };
+
+  const beginTimelapse = () => {
+    if (timeouts.current.length) clearTimeouts();
+
+    let j = 0;
+    for (let i = MIN_YEAR; i <= MAX_YEAR; ++i) {
+      timeouts.current.push(
+        setTimeout(() => {
+          setYear(i);
+          setTransitionDuration(TIMELAPSE_INTERVAL);
+          setAltitude(() => getAltitudeCalculationFunction(i));
+        }, j * TIMELAPSE_INTERVAL)
+      );
+      ++j;
+    }
+    setTimeout(() => {
+      timeouts.current = [];
+    }, j * TIMELAPSE_INTERVAL);
+  };
+
   return (
     <div className="App">
+      <div className="play-display">
+        <IconButton
+          aria-label="play"
+          onClick={beginTimelapse}
+          sx={{
+            paddingRight: 2,
+          }}
+        >
+          <PlayCircle
+            sx={{
+              width: 100,
+              height: 100,
+            }}
+          />
+        </IconButton>
+        <div className="current-year">{year}</div>
+        <div className="slider-container">
+          <Slider
+            aria-label="year"
+            value={year}
+            onChange={changeYear}
+            step={1}
+            min={MIN_YEAR}
+            max={MAX_YEAR}
+            valueLabelDisplay="auto"
+          />
+        </div>
+      </div>
       <Globe
         ref={globeEl}
-        // globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        // globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
         polygonsData={countries.features.filter(
           (d) => d.properties.ISO_A2 !== "AQ"
         )}
         polygonAltitude={altitude}
-        polygonCapColor={() => "rgba(200, 0, 0, 0.6)"}
-        polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+        polygonCapColor={() => "rgba(128,128,128, 0.9)"}
+        polygonSideColor={() => "rgba(128,128,128, 0.15)"}
         polygonLabel={({ properties: d }) => `
         <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-        CO2 Emissions per capita: <i>${
-          Math.round(+d.CO2_DATA_BY_YEAR?.[2018]?.co2_per_capita * 1000) / 1000
+        CO2 Emissions: <i>${
+          Math.round(+d.CO2_DATA_BY_YEAR?.[year]?.co2 * 1000) / 1000
         } (tons)</i>
       `}
         polygonsTransitionDuration={transitionDuration}
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const MIN_YEAR = 1920;
+export const MAX_YEAR = 2020;
+
+export const TIMELAPSE_INTERVAL = 200;
+
+export const co2Variables = {
+  co2: {},
+  co2_criteria: {},
+};
